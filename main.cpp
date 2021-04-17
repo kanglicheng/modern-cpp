1017705fe075043453e19187ac1334ba48931577
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2021-04-17 18:56:05 +0000

    stacks and queues

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
 using std::cout;
+using std::endl; 
 
 int main() {
-  cout << "Hello World!\n";
-  int *numPtr = new int; 
-  cout << numPtr; 
+  // cout << "Identify what the following statements are printing\n";
+  // int *numPtr = new int; 
+  // cout << "*numPtr: " <<*numPtr << std::endl;
+  // cout << "numPtr: " << numPtr << std::endl; 
+  // cout << "&numPtr: " << &numPtr << std:: endl; 
 
+  // *numPtr = 42;
+  // // what happens after the above assignment?
+  // cout << "*numPtr: " <<*numPtr << std::endl;
+  // cout << "numPtr: " << numPtr << std::endl; 
+  // cout << "&numPtr: " << &numPtr << std:: endl; 
+
+  // int i = 2; 
+  // int *p = &i;
+  // int j = 5;
+  // p = &j; 
+
+  // cout << p << std::endl;
+  // cout << *p << std::endl;   
+
+ int *i = new int; 
+ *i = 0;
+ int &j = *i; 
+ j++; 
+ cout << j <<endl; 
+ cout <<&j; 
+
+  
 }
